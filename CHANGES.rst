b02c25a49752e21c4ef5df2f9f2b97c32a47ddfc
perrygreenfield <perry@stsci.edu>
2021-02-11 12:34:23 -0500

    initial setup

---
+++ CHANGES.rst
@@ -0,0 +1,4 @@
+0.1.0 (unreleased)
+==================
+
+- Initial Schemas for Roman Calibration Pipeline and SDP file generation
---
+++ src/integration.py
@@ -0,0 +1,27 @@
+import sys
+
+from asdf.resource import DirectoryResourceMapping
+
+if sys.version_info < (3, 9):
+    import importlib_resources
+else:
+    import importlib.resources as importlib_resources
+
+
+def get_resource_mappings():
+    """
+    Get the resource mapping instances for the datamodel schemas
+    and manifests.  This method is registered with the
+    asdf.resource_mappings entry point.
+
+    Returns
+    -------
+    list of collections.abc.Mapping
+    """
+    from . import resources
+    resources_root = importlib_resources.files(resources)
+
+    return [
+        DirectoryResourceMapping(resources_root / "schemas", "http://stsci.edu/schemas/datamodels/roman", recursive=True),
+        DirectoryResourceMapping(resources_root / "manifests", "http://stsci.edu/asdf/datamodels/roman/manifests"),
+    ]
