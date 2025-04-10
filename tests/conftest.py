cc96e444035b793459326c2fdb7cac9960cdba77
William Jamieson <william@jamieson.phd>
2025-04-10 19:00:10 -0400

    Update the tests to search the latest schemas directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
+import importlib.resources as importlib_resources
+from itertools import chain
+
 import asdf
 import pytest
 import yaml
+
+from rad import resources
 
 MANIFEST_URIS = [
     "asdf://stsci.edu/datamodels/roman/manifests/datamodels-1.0",
@@ -19,4 +24,17 @@ def manifest_entry(request):
 
 @pytest.fixture(scope="session", params=MANIFESTS)
 def manifest(request):
+    return request.param
+
+
+@pytest.fixture(
+    params=chain(
+        (importlib_resources.files(resources) / "schemas").glob("**/*.yaml"),
+        (importlib_resources.files(resources) / "latest").glob("**/*.yaml"),
+    )
+)
+def schema_path(request):
+    """
+    Get the schema path for the test.
+    """
     return request.param
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,10 +3,12 @@ Test that the asdf library integration is working properly.
 """
 
 import importlib.resources as importlib_resources
+from pathlib import Path
 
 import asdf
 import pytest
 import yaml
+from semantic_version import Version
 
 from rad import resources
 
@@ -19,7 +21,6 @@ def test_manifest_integration(manifest_path):
     assert asdf_content == content
 
 
-@pytest.mark.parametrize("schema_path", (importlib_resources.files(resources) / "schemas").glob("**/*.yaml"))
 def test_schema_integration(schema_path):
     content = schema_path.read_bytes()
     schema = yaml.safe_load(content)
@@ -27,11 +28,34 @@ def test_schema_integration(schema_path):
     assert asdf_content == content
 
 
-@pytest.mark.parametrize("schema_path", (importlib_resources.files(resources) / "schemas").glob("**/*.yaml"))
 def test_schema_filename(schema_path):
     """
     Check the filename pattern aligns with the schema ID.
     """
+    prefix = importlib_resources.files(resources)
+    id_suffix = str(schema_path.with_suffix("")).split(str(prefix))[-1]
+
     schema = yaml.safe_load(schema_path.read_bytes())
-    id_suffix = str(schema_path.with_suffix("")).split(str(importlib_resources.files(resources)))[-1]
-    assert schema["id"].endswith(id_suffix)
+    if "schemas" in str(schema_path) or "manifests" in str(schema_path):
+        assert schema["id"].endswith(id_suffix)
+
+        if "schemas" in str(schema_path):
+            # check that there is a latest version of the schema
+            latest_path = (
+                Path(str(schema_path).split("schemas")[0]) / "latest" / f"{id_suffix.split('/schemas')[1][1:].split('-')[0]}.yaml"
+            )
+            assert latest_path.exists(), latest_path
+            latest = yaml.safe_load(latest_path.read_bytes())
+
+            # Check that the version number is lower than the latest version
+            assert Version(schema["id"].split("-")[1]) < Version(latest["id"].split("-")[1])
+    else:
+        # latest schemas case -> filename "../latest/.." should instead be "../schemas/.."
+        # and the version number should be missing
+        suffix = "/".join(id_suffix.split("/")[2:])
+        id_suffix = f"/schemas/{suffix}"
+        assert schema["id"].split("-")[0].endswith(id_suffix)
+
+        # There should not be a schemas file with the exact version number used
+        # by latest
+        assert not (prefix / f"{id_suffix}-{schema['id'].split('-')[1]}.yaml").exists()
