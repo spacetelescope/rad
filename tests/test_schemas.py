2919fca7e75fae7e917881eaf8f839c861b2fed6
William Jamieson <william@jamieson.phd>
2025-07-21 16:05:52 -0400

    Bump l3_common for refactoring

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -22,14 +22,11 @@ VARCHAR_XFAILS = (
 
 REF_COMMON_XFAILS = ("asdf://stsci.edu/datamodels/roman/schemas/reference_files/skycells-1.0.0",)
 
-ARRAY_TAG_XFAILS = (
-    "asdf://stsci.edu/datamodels/roman/schemas/l1_detector_guidewindow-1.0.0",
-    "asdf://stsci.edu/datamodels/roman/schemas/l1_detector_guidewindow-1.1.0",
-)
+ARRAY_TAG_XFAILS = ("asdf://stsci.edu/datamodels/roman/schemas/l1_detector_guidewindow-1.1.0",)
 
-REQUIRED_SKIPS = ("asdf://stsci.edu/datamodels/roman/schemas/wfi_mosaic-1.3.0",)
+REQUIRED_SKIPS = ("asdf://stsci.edu/datamodels/roman/schemas/wfi_mosaic-1.4.0",)
 
-NESTED_REQUIRED_SKIPS = ("asdf://stsci.edu/datamodels/roman/schemas/l3_common-1.0.0",)
+NESTED_REQUIRED_SKIPS = ("asdf://stsci.edu/datamodels/roman/schemas/l3_common-1.1.0",)
 
 
 class TestSchemaContent:
